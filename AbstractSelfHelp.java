59f6d2a43c05529b504c3e980130071f8baa254c
shusrushabezugam <sbezugam@villanova.edu>
2017-07-08 20:41:01 -0400

    AbstractSelfhelp class

--- AbstractSelfHelp.java
+++ AbstractSelfHelp.java
@@ -4,14 +4,7 @@ public class AbstractSelfHelp
 public enum SelfHelpDescription{HELPED_A_LOT,HELPED_A_LITTLE,DIDNT_HELP,MADE_IT_WORSE};
 
 
-public Map<SelfHelpDescription,String> getSelfHelp()
-{
-	 EnumMap<SelfHelpDescription, String> map = new EnumMap<SelfHelpDescription, String>(SelfHelpDescription.class);  
-	   map.put(SelfHelpDescription.HELPED_A_LOT, "4");  
-	   map.put(SelfHelpDescription.HELPED_A_LITTLE, "2");  
-	   map.put(SelfHelpDescription.DIDNT_HELP, "0");  
-	   map.put(SelfHelpDescription.MADE_IT_WORSE,"-2");
-	return map;
-}
+public abstract Map<SelfHelpDescription,String> getSelfHelp();
+
 
 }
